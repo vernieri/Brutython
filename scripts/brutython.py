c576777bf00abaec20174458189def37b2e14b6c
Vernieri <jvmendes.ti@gmail.com>
2019-03-20 06:57:20 -0700

    Create brutython.py

---
+++ scripts/brutython.py
@@ -0,0 +1,18 @@
+import os
+import requests
+from time import sleep
+from random import randint
+
+#path = '/root/Documents/scripts' #Poe o Path de exec
+#lista = 'user_lista.txt' #Poe tua lista
+#url = 'http://www.google.com' #Sua_Url se tu for usar estatico
+
+def starter():
+	print('Bem-Vindo ao Brutython, Url bruteforce feito em Python por @SeuNick')
+	print('Altere diretamente no script se for mais de um argumento!\n')
+
+	lista = raw_input('Passa o path da lista: ')
+	url = raw_input('Digite a URL: ')
+	variavel = raw_input('Campo que iremos Bruceforciar: ')
+	str(variavel)
+	leitura(url, variavel, lista)
